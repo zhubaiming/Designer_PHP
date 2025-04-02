b01246e2a103cd3ee88a2ba3797802374a35c302
zhubaiming <other.zhubaiming@gmail.com>
2025-04-02 13:25:08 +0800

    添加json打包器

---
+++ src/Packers/BodyPacker.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hongyi\Designer\Packers;
+
+use Hongyi\Designer\Contracts\PackerInterface;
+
+class BodyPacker implements PackerInterface
+{
+    public function pack(array $parameters): string
+    {
+        return json_encode($parameters, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+    }
+
+    public function unpack(string $payload): ?array
+    {
+        $result = json_decode($payload, true);
+
+        return is_array($result) ? $result : null;
+    }
+}
