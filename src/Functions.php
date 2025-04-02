73a7a61746ef4b76150a648f6df1bdb902587b7c
zhubaiming <other.zhubaiming@gmail.com>
2025-04-02 13:15:50 +0800

    添加辅助函数

--- src/Functions.php
+++ src/Functions.php
@@ -2,6 +2,9 @@
 
 declare(strict_types=1);
 
+use Hongyi\Designer\Providers\ConfigServiceProvider;
+use Hongyi\Designer\Vaults;
+
 if (!function_exists('filter_parameters')) {
     function filter_parameters(array $parameters, \Closure $closure = null): array
     {
@@ -19,7 +22,7 @@ if (!function_exists('get_radar_method')) {
 if (!function_exists('get_radar_url')) {
     function get_radar_url(array $parameters): ?string
     {
-        return isset($parameters['_url']) ? strtoupper($parameters['_url']) : null;
+        return $parameters['_url'] ?? null;
     }
 }
 
@@ -27,5 +30,24 @@ if (!function_exists('get_radar_body')) {
     function get_radar_body(array $parameters): ?string
     {
         return isset($parameters['_body']) ? strtoupper($parameters['_url']) : null;
+    }
+}
+
+if (!function_exists('random_nonce')) {
+    /**
+     * @throws \Random\RandomException
+     */
+    function random_nonce(int $length): string
+    {
+        return strtoupper(substr(bin2hex(random_bytes($length)), 0, $length));
+    }
+}
+
+if (!function_exists('get_config')) {
+    function get_config(string $channel, string $type = 'default', array $params = [])
+    {
+        $config = Vaults::get(ConfigServiceProvider::class)->get($channel);
+
+        return $config[$type] ?? $params;
     }
 }
