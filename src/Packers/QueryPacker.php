e13e6772d2aa722478f896ecbb9cdb256ff446a0
zhubaiming <other.zhubaiming@gmail.com>
2025-04-02 13:25:43 +0800

    添加其他打包器

---
+++ src/Packers/QueryPacker.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hongyi\Designer\Packers;
+
+use Hongyi\Designer\Contracts\PackerInterface;
+
+class QueryPacker implements PackerInterface
+{
+    public function pack()
+    {
+        // TODO: Implement pack() method.
+    }
+
+    public function unpack()
+    {
+        // TODO: Implement unpack() method.
+    }
+}
