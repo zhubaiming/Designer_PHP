f0e4c08f4a9dabd83e5765557f7a9f0f7252948f
zhubaiming <other.zhubaiming@gmail.com>
2025-04-02 13:28:35 +0800

    添加装载配置的具体服务执行类

---
+++ src/Services/Config.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hongyi\Designer\Services;
+
+use Hongyi\Designer\Exceptions\InvalidConfigException;
+
+class Config
+{
+    private array $items = [];
+
+    public function __construct(mixed $items = [])
+    {
+        $this->items = $items;
+    }
+
+    public function get($id)
+    {
+        $value = $this->items;
+
+        if (!str_contains($id, '.')) {
+            if (!array_key_exists($id, $value)) {
+                throw new InvalidConfigException();
+            }
+
+            return $value[$id];
+        } else {
+            foreach (explode('.', $id) as $key) {
+                if (!is_array($value) || !array_key_exists($key, $value)) {
+                    throw new InvalidConfigException();
+                }
+
+                $value = $value[$key];
+            }
+        }
+
+        return $value;
+    }
+}
