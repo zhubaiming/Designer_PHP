12e0505b06810fff6f7a983778a9bf52c993970a
zhubaiming <other.zhubaiming@gmail.com>
2025-04-02 13:27:17 +0800

    添加起始插件

---
+++ src/Plugins/StartPlugin.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hongyi\Designer\Plugins;
+
+use Hongyi\Designer\Contracts\PluginInterface;
+use Hongyi\Designer\Patchwerk;
+
+/**
+ * 起始插件
+ *
+ * 作用: 将「输入参数」赋值到「原始输入参数 」
+ */
+class StartPlugin implements PluginInterface
+{
+    public function handle(Patchwerk $patchwerk, \Closure $next): Patchwerk
+    {
+        $patchwerk->setParametersOrigin($patchwerk->getParameters());
+
+        return $next($patchwerk);
+    }
+}
