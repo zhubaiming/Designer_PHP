070cd2c21dff94d4e8e23a2ad15e2d3903812900
zhubaiming <other.zhubaiming@gmail.com>
2025-04-02 13:20:13 +0800

    添加插件统一接口

---
+++ src/Contracts/PluginInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hongyi\Designer\Contracts;
+
+use Hongyi\Designer\Patchwerk;
+
+/**
+ * 插件
+ * 遵循 pipeline 的实现方式，内部定义每个插件的具体执行内容
+ */
+interface PluginInterface
+{
+    public function handle(Patchwerk $patchwerk, \Closure $next): Patchwerk;
+}
